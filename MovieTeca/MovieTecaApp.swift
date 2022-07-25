ce791fbf001f6a3dadbbbd50ef8240ccc3ba3f40
Manuel E. Hernandez <kicke10h@gmail.com>
2022-07-24 20:23:53 -0500

    Initial Commit

---
+++ MovieTeca/MovieTecaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTecaApp.swift
+//  MovieTeca
+//
+//  Created by Kike Hernandez D. on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieTecaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
