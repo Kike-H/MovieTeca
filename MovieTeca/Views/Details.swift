24aba9a091852e1cf914cd42e947538c50468fcf
Manuel E. Hernandez <kicke10h@gmail.com>
2022-07-28 19:10:00 -0500

    Making details interfcae

--- MovieTeca/Views/Details.swift
+++ MovieTeca/Views/Details.swift
@@ -7,13 +7,38 @@
 
 import SwiftUI
 
+
 struct Details: View {
+    let starts: [Int] = [1,2,3,4,5]
     var body: some View {
-        ScrollView {
-            VStack {
-                
-            }
-        }
+        NavigationView {
+            ScrollView {
+                VStack{
+                    HStack {
+                        Card(width: 140, height: 200, padding: 12, color: .indigo)
+                        VStack {
+                            Text("Movie Title")
+                                .font(.title)
+                            Text("Movie title 2")
+                                .font(.title3)
+                                .padding(.vertical, 1)
+                            HStack {
+                                ForEach(starts, id: \.self) { start in
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.indigo)
+                                }
+                            }
+                        }
+                        Spacer()
+                    }
+                    SeccionTitle(title: "Synopsis")
+                    Text("lorem ipsum")
+                    SeccionTitle(title: "Actors")
+                    Carousel(width: 130, height: 170, padding: 12)
+                } //Vstack
+            } // Scroll view
+            .navigationTitle(Text("Information"))
+        }// Navigation View
     }
 }
 
